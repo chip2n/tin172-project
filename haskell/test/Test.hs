cb22589fec9a8a1b1568dc13c12d38bfee08d248
Sebastian Lagermann <barafinkel@gmail.com>
2014-05-12 11:56:39 +0200

    Really finished the tests for validate.

--- haskell/test/Test.hs
+++ haskell/test/Test.hs
@@ -48,7 +48,6 @@ main = defaultMainWithOpts
         , testCase "validateObjectTest5" validateObjectTest5
         , testCase "validateObjectTest6" validateObjectTest6
         , testCase "validateObjectTest7" validateObjectTest7
-        , testCase "validateObjectTest8" validateObjectTest8
         ]
     ] mempty
 
@@ -152,25 +151,21 @@ findObjPosTest2 = assertBool "Object with id \"g\" was not found in column 1 and
 findObjPosTest3 :: Assertion
 findObjPosTest3 = assertBool "Object with id \"l\" was not found in column 1 and height 1" ((findObjPos "l" (world startState)) == Just (1,1))
 
--- | Test if a large ball can be placed in a large box
-validateObjectTest1 :: Assertion
-validateObjectTest1 = assertBool "The large ball can be placed in a large box" $ not $ validate largeWhiteBall largeRedBox
-
 -- | Tests if we can't place something on a ball
-validateObjectTest2 :: Assertion
-validateObjectTest2 = assertBool "I managed to place a large box on a large ball" $ not $ validate largeRedBox largeWhiteBall
+validateObjectTest1 :: Assertion
+validateObjectTest1 = assertBool "I managed to place a large box on a large ball" $ not $ validate largeRedBox largeWhiteBall
 
 -- | Tests if placing a large object on a small object returns false
-validateObjectTest3 :: Assertion
-validateObjectTest3 = assertBool "I managed to place a large object on a small object" $ not $ validate largeRedBox smallGreenBox
+validateObjectTest2 :: Assertion
+validateObjectTest2 = assertBool "I managed to place a large object on a small object" $ not $ validate largeRedBox smallGreenBox
 
 -- | Testa whether we can place a large plank on top of a small brick
-validateObjectTest4 :: Assertion
-validateObjectTest4 = assertBool "I managed to place a large plank on a small brick" $ not $ validate (Object Large Red Plank) (Object Small White Brick)
+validateObjectTest3 :: Assertion
+validateObjectTest3 = assertBool "I managed to place a large plank on a small brick" $ not $ validate (Object Large Red Plank) (Object Small White Brick)
 
 -- | Checks that nothing can be placed on a ball
-validateObjectTest5 :: Assertion
-validateObjectTest5 = assertBool "I managed to place something on a ball"
+validateObjectTest4 :: Assertion
+validateObjectTest4 = assertBool "I managed to place something on a ball"
   $ and $ map f $ map xs forms
     where
       f :: Object -> Bool
@@ -180,8 +175,8 @@ validateObjectTest5 = assertBool "I managed to place something on a ball"
       xs x = (Object Large White x)
 
 -- | Checks if every large object cannot be placed on every small object
-validateObjectTest6 :: Assertion
-validateObjectTest6 = assertBool "I managed to place a large object on a small one"
+validateObjectTest5 :: Assertion
+validateObjectTest5 = assertBool "I managed to place a large object on a small one"
   $ and $ [(f l s) |  l <- map (oc Large) forms, s <- map (oc Small) forms]
     where
       f :: Object -> Object -> Bool
@@ -191,10 +186,11 @@ validateObjectTest6 = assertBool "I managed to place a large object on a small o
       oc sz frm = (Object sz White frm)
 
 -- | Boxes cannot contain pyramids or planks of the same size
-validateObjectTest7 :: Assertion
-validateObjectTest7 = assertBool "I managed to place a plank/pyramid inside a box"
+validateObjectTest6 :: Assertion
+validateObjectTest6 = assertBool "I managed to place a plank/pyramid inside a box"
   $ and $ [(f p (oc Large Box)) | p <- map (oc Large) [Pyramid,Plank]]
        ++ [(f p (oc Small Box)) | p <- map (oc Small) [Pyramid,Plank]]
+       ++ [(validate p (oc Large Box)) | p <- map (oc Small) [Pyramid,Plank]]
     where
       f :: Object -> Object -> Bool
       f po bo = not $ validate po bo
@@ -203,15 +199,15 @@ validateObjectTest7 = assertBool "I managed to place a plank/pyramid inside a bo
       oc sz frm = (Object sz White frm)
 
 --Boxes can only be supported by tables or planks of the same size, but large boxes can also be supported by large bricks
-validateObjectTest8 :: Assertion
-validateObjectTest8 = assertBool "I managed to place a box on something other than a table or plank (or large brick for large box)"
-  $ and $ [(f (oc Large Box) p) | p <- map (oc Large) [Table,Plank,Brick]]
-       ++ [(f (oc Small Box) p) | p <- map (oc Small) [Table,Plank]]
-       ++ [(not $ f (oc Small Box) p) | p <- map (oc Large) [Table,Plank,Brick]]
-       ++ [(not $ f (oc Large Box) p) | p <- map (oc Small) [Table,Plank]]
+validateObjectTest7 :: Assertion
+validateObjectTest7 = assertBool "I managed to place a box on something other than a table or plank (or large brick for large box)"
+  $ and $ [(validate (oc Large Box) p) | p <- map (oc Large) [Table,Plank,Brick]]
+       ++ [(validate (oc Small Box) p) | p <- map (oc Small) [Table,Plank]]
+       ++ [(not $ validate (oc Small Box) p) | p <- map (oc Large) [Table,Plank,Brick]]
+       ++ [(not $ validate (oc Large Box) p) | p <- map (oc Small) [Table,Plank]]
     where
-      f :: Object -> Object -> Bool
-      f bo o = not $ validate bo o
+      --f :: Object -> Object -> Bool
+      --f bo o = validate bo o
 
       oc :: Size -> Form -> Object
       oc sz frm = (Object sz White frm)
