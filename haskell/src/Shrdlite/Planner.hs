4139e106ffc3b4afd51e1890ecf2f79556578eaf
Sebastian Lagermann <barafinkel@gmail.com>
2014-05-18 12:14:55 +0200

    Cleaned up the god and made it more readable.

--- haskell/src/Shrdlite/Planner.hs
+++ haskell/src/Shrdlite/Planner.hs
@@ -1,11 +1,11 @@
 module Shrdlite.Planner where
 
+import Shrdlite.AStar
 import Shrdlite.Common
 import Shrdlite.Grammar
-import Shrdlite.AStar
 
-import Data.Maybe
 import Control.Monad
+import Data.Maybe
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -16,25 +16,24 @@ import qualified Data.Set as S
 -- returns Nothing
 solve :: World -> Maybe Id -> Objects -> [Goal] -> Maybe Plan
 solve _ _ _ []     = Nothing
-solve w h o (g:[]) = plan
-  where
-    plan = aStar (worldGraph o) (heuristics g) (check g) (w,h)
+solve w h o (g:[]) = let plan = aStar (worldGraph o) (heuristics g) (check g) (w,h)
+                     in plan
 solve w h o (g:gs) = case plan of
                         Nothing -> Nothing
-                        Just p  ->  case newPlan of
-                           Nothing -> Nothing
-                           Just p' -> Just $ p ++ p'
-                           where (newWorld, newHolding) = simulatePlan w h p
-                                 newPlan = solve newWorld newHolding o gs
+                        Just p  -> case newPlan of
+                          Nothing -> Nothing
+                          Just p' -> Just $ p ++ p'
+                          where (newWorld, newHolding) = simulatePlan w h p
+                                newPlan = solve newWorld newHolding o gs
   where
     plan = aStar (worldGraph o) (heuristics g) (check g) (w,h)
 
 simulatePlan :: World -> Maybe Id -> Plan -> (World, Maybe Id)
-simulatePlan oldWorld h [] = (oldWorld, h)
+simulatePlan oldWorld h        []     = (oldWorld, h)
 simulatePlan oldWorld (Just h) (p:ps) = simulatePlan newWorld Nothing ps
    where (left, col:right) = L.splitAt (read (last (words p))::Int) oldWorld
          newWorld = left ++ [col ++ [h]] ++ right
-simulatePlan oldWorld Nothing (p:ps) = simulatePlan newWorld (Just c) ps
+simulatePlan oldWorld Nothing (p:ps)  = simulatePlan newWorld (Just c) ps
    where (left, (c:col):right) = L.splitAt (read (last (words p))::Int) oldWorld
          newWorld = left ++ col:right
 
@@ -44,13 +43,15 @@ worldGraph o (w, h) = case h of
   Just i  -> foldr (placeObject o i) S.empty worldParts
   Nothing -> foldr (takeObject o) S.empty worldParts
   where
-    worldParts = init $ zip (L.inits w) (L.tails w) -- TODO: try highest column first, so sort the zipped lists
+    worldParts = init $ zip (L.inits w) (L.tails w)
 
--- | Puts the World parts together, with the Id added to the beginning of the second list
-placeObject :: Objects -> Id -> ([[Id]], [[Id]]) -> S.Set WorldHolding -> S.Set WorldHolding
+-- | Puts the World parts together, with the Id added to the beginning of
+-- the second list
+placeObject :: Objects -> Id -> ([[Id]], [[Id]]) -> S.Set WorldHolding
+            -> S.Set WorldHolding
 placeObject o h e s = case newWorld h of
   Nothing -> s
-  Just w -> S.insert (w, Nothing) s
+  Just w  -> S.insert (w, Nothing) s
   where
     newWorld elm = joinModified o e (\x -> x ++ [elm])
 
@@ -81,8 +82,8 @@ maybeLast xs = Just $ last xs
 -- list) and is normally either placing or taking an object from the top of
 -- the list. Returns Just a new world
 joinModified :: Objects -> ([[Id]], [[Id]]) -> ([Id] -> [Id]) -> Maybe [[Id]]
-joinModified _ ([], []) _ = Nothing
-joinModified _ (_, []) _ = Nothing
+joinModified _    ([], [])   _ = Nothing
+joinModified _    (_,  [])   _ = Nothing
 joinModified objs (xs, y:ys) f = case val objs (f y) of
    Nothing -> Nothing
    Just y' -> Just $ xs ++ y':ys
@@ -91,28 +92,28 @@ joinModified objs (xs, y:ys) f = case val objs (f y) of
 -- order. If there are less than two object we assume there can be no conflicts.
 val :: Objects -> [Id] -> Maybe [Id]
 val objs y' | length y' > 1 = case l y1 of
+                Nothing  -> Nothing
+                Just y1' -> case l y2 of
                   Nothing  -> Nothing
-                  Just y1' -> case l y2 of
-                     Nothing  -> Nothing
-                     Just y2' -> if validate y1' y2'
-                                 then Just y'
-                                 else Nothing
-             | otherwise = Just y'
-      where l ys'  = M.lookup ys' objs
-            [y1, y2] = take 2 $ reverse y'
+                  Just y2' -> if validate y1' y2'
+                                then Just y'
+                                else Nothing
+            | otherwise = Just y'
+  where l ys'    = M.lookup ys' objs
+        [y1, y2] = take 2 $ reverse y'
 
 -- | Heuristic defining how good a state is
 heuristics :: Goal -> WorldHolding -> Int
 heuristics (TakeGoal obj) (w, h) = case obj of
-  Flr -> error "Take floor goal cannot be assessed"
+  Flr       -> error "Take floor goal cannot be assessed"
   (Obj _ i) -> case h of
-    Nothing             -> 1 + 2 * fromMaybe (error "object is not in the world")
-                                   (idHeight i w)
-    Just holdingId      ->
+    Nothing        -> 1 + 2 * fromMaybe (error "object is not in the world")
+                                        (idHeight i w)
+    Just holdingId ->
       if i == holdingId
-      then 0
-      else 2 + 2 * fromMaybe (error "object is not in the world")
-                   (idHeight i w)
+        then 0
+        else 2 + 2 * fromMaybe (error "object is not in the world")
+                               (idHeight i w)
 heuristics (MoveGoal Ontop (Obj _ i) Flr) (w,h) = if getFloorSpace w
   then case h of
          Nothing -> if hght == 0 then 0
@@ -125,57 +126,63 @@ heuristics (MoveGoal Ontop (Obj _ i) Flr) (w,h) = if getFloorSpace w
       Nothing ->  if hght == 0
                     then 0
                     else 2 * hght + minimum fs
-      Just holdingId | i == holdingId -> 3 + minimum fs -- put it down, move the smallest pile and then pick it up and put it on the floor.
-                     | hght == 0      -> 0 -- Is our object already on the floor? good
-                     | otherwise      -> 3 + 2 * oAbove + minimum fs -- put the holding down, clear the objects above, move the smallest pile.
-  where oAbove = fromMaybe (error "object isn't in the world") (idHeight i w)
-        hght = fromMaybe (error "planner: where did the object go?") (objPos i w)
-        fs = makeFloorSpace w
+-- put it down, move the smallest pile and then pick it up and put it on the floor
+      Just holdingId | i == holdingId -> 3 + minimum fs
+                     -- Is our object already on the floor? good
+                     | hght == 0      -> 0 
+          -- put the holding down, clear the objects above, move the smallest pile
+                     | otherwise      -> 3 + 2 * oAbove + minimum fs 
+  where
+    oAbove = fromMaybe (error "object isn't in the world") (idHeight i w)
+    hght = fromMaybe (error "planner: where did the object go?") (objPos i w)
+    fs = makeFloorSpace w
 heuristics g@(MoveGoal rel (Obj _ i1) (Obj _ i2)) (w,h) =
   if check g (w,h) -- if goal fulfilled, it has to be 0
-  then 0
-  else case rel of
-  Ontop ->  let h1 = fromMaybe 0 $ liftM (*2) $ idHeight i1 w
-                h2 = fromMaybe 0 $ liftM (*2) $ idHeight i2 w
-            in case h of
-                 Nothing        -> 2 + h1 + h2
-                 Just holdingId
-                   | holdingId == i1  -> 1 + h1 + h2 -- put the object down
-                   | otherwise  -> 3 + h1 + h2 -- put the holding object down and move object 1 ontop
-  Inside -> let h1 = fromMaybe 0 $ liftM (*2) $ idHeight i1 w
-                h2 = fromMaybe 0 $ liftM (*2) $ idHeight i2 w
-            in case h of
-                 Nothing        -> if check g (w,h)
-                                   then 0
-                                   else 2 + h1 + h2
-                 Just holdingId
-                   | holdingId == i1  -> 1 + h1 + h2 -- put the object down
-                   | otherwise  -> 3 + h1 + h2 -- put the holding object down and move object 1 ontop
-  Beside -> case h of
-    Nothing        -> cheapestCost i1 i2 w
-    Just holdingId -> if holdingId == i1 || holdingId == i2
-                        then 1
-                        else 2 + cheapestCost i1 i2 w
-  Leftof -> case h of
-    Nothing        -> cheapestCost i1 i2 w
-    Just holdingId -> if holdingId == i1 || holdingId == i2
-                        then 1
-                        else 2 + cheapestCost i1 i2 w
-  Rightof -> case h of
-    Nothing        -> cheapestCost i1 i2 w
-    Just holdingId -> if holdingId == i1 || holdingId == i2
-                        then 1
-                        else 2 + cheapestCost i1 i2 w
-  Above -> case h of
-    Nothing        -> cheapestCost i1 i2 w
-    Just holdingId -> if holdingId == i1 || holdingId == i2
-                        then 1
-                        else 2 + cheapestCost i1 i2 w
-  Under -> case h of
-    Nothing        -> cheapestCost i1 i2 w
-    Just holdingId -> if holdingId == i1 || holdingId == i2
-                        then 1
-                        else 2 + cheapestCost i1 i2 w
+    then 0
+    else case rel of
+      Ontop ->  let h1 = fromMaybe 0 $ liftM (*2) $ idHeight i1 w
+                    h2 = fromMaybe 0 $ liftM (*2) $ idHeight i2 w
+                in case h of
+                     Nothing        -> 2 + h1 + h2
+                     Just holdingId
+                       | holdingId == i1  -> 1 + h1 + h2 -- put the object down
+                       -- put the holding object down and move object 1 ontop
+                       | otherwise  -> 3 + h1 + h2 
+      Inside -> let h1 = fromMaybe 0 $ liftM (*2) $ idHeight i1 w
+                    h2 = fromMaybe 0 $ liftM (*2) $ idHeight i2 w
+                in case h of
+                     Nothing        -> if check g (w,h)
+                                         then 0
+                                         else 2 + h1 + h2
+                     Just holdingId
+                       | holdingId == i1  -> 1 + h1 + h2 -- put the object down
+                       -- put the holding object down and move object 1 ontop
+                       | otherwise  -> 3 + h1 + h2 
+      Beside -> case h of
+        Nothing        -> cheapestCost i1 i2 w
+        Just holdingId -> if holdingId == i1 || holdingId == i2
+                            then 1
+                            else 2 + cheapestCost i1 i2 w
+      Leftof -> case h of
+        Nothing        -> cheapestCost i1 i2 w
+        Just holdingId -> if holdingId == i1 || holdingId == i2
+                            then 1
+                            else 2 + cheapestCost i1 i2 w
+      Rightof -> case h of
+        Nothing        -> cheapestCost i1 i2 w
+        Just holdingId -> if holdingId == i1 || holdingId == i2
+                            then 1
+                            else 2 + cheapestCost i1 i2 w
+      Above -> case h of
+        Nothing        -> cheapestCost i1 i2 w
+        Just holdingId -> if holdingId == i1 || holdingId == i2
+                            then 1
+                            else 2 + cheapestCost i1 i2 w
+      Under -> case h of
+        Nothing        -> cheapestCost i1 i2 w
+        Just holdingId -> if holdingId == i1 || holdingId == i2
+                            then 1
+                            else 2 + cheapestCost i1 i2 w
 
 -- | Returns the cheapest heuristics to move all objects above one of the
 -- two given objects.
@@ -186,26 +193,26 @@ cheapestCost i1 i2 w = (*) 2 $ minimum [h1,h2]
 
 -- | Checks whether there exits an empty floor space.
 getFloorSpace :: World -> Bool
-getFloorSpace [] = False
+getFloorSpace []     = False
 getFloorSpace ([]:_) = True
 getFloorSpace (_:ws) = getFloorSpace ws
 
 -- | Returns the number of elements in every column.
 makeFloorSpace :: World -> [Int]
-makeFloorSpace [] = error "Planner.makeFloorSpace: Your world seems to be empty"
-makeFloorSpace [w] = [length w]
+makeFloorSpace []     = error "Planner.makeFloorSpace: World seems to be empty"
+makeFloorSpace [w]    = [length w]
 makeFloorSpace (w:ws) = 2 * length w : makeFloorSpace ws
 
 -- | Returns the column a given object exists in.
 clmn :: Int -> Id -> World -> Maybe Int
-clmn _ _ [] = Nothing
+clmn _   _ []     = Nothing
 clmn row i (w:ws) = case L.elemIndex i w of
                       Nothing -> clmn (row+1) i ws
                       Just _  -> return row
 
 -- | Gives an int for how far down the object is
 idHeight :: Id -> World -> Maybe Int
-idHeight _ [] = Nothing
+idHeight _ []     = Nothing
 idHeight i (w:ws) = case idHeight' i w of
   Nothing -> idHeight i ws
   mVal    -> mVal
@@ -214,14 +221,14 @@ idHeight i (w:ws) = case idHeight' i w of
 -- given object if it exits.
 idHeight' :: Id -> [Id] -> Maybe Int
 idHeight' i ws = case L.elemIndex i ws of
-  Nothing -> Nothing
+  Nothing    -> Nothing
   Just index -> return $ (length ws - 1) - index
 
 -- | Returns an objects height in a column where 0 is the bottom.
 objPos :: Id -> World -> Maybe Int
-objPos _ [] = Nothing
+objPos _ []     = Nothing
 objPos i (w:ws) = case L.elemIndex i w of
-  Nothing -> objPos i ws
+  Nothing    -> objPos i ws
   Just index -> return index
 
 -- | Checks if the first object is over the second.
@@ -242,36 +249,36 @@ isOver i1 i2 above (w:ws) = case L.elemIndex i1 w of
 -- | Checks if the goal is fulfilled in the provided state
 check :: Goal -> WorldHolding -> Bool
 check goal (w, h) = case goal of
-  TakeGoal Flr  -> error "Take floor goal cannot be assessed"
+  TakeGoal Flr        -> error "Take floor goal cannot be assessed"
   TakeGoal (Obj _ i)  -> case h of
     Nothing             -> False
     Just holdingId      -> i == holdingId
   MoveGoal Ontop (Obj _ i) Flr -> case objPos i w of
     Just height -> height == 0
-    Nothing -> False
+    Nothing     -> False
   MoveGoal rel (Obj _ i1) (Obj _ i2) -> case rel of
-    Ontop -> isOver i1 i2 True w
-    Inside -> isOver i1 i2 True w
-    Beside -> isBeside i1 i2 w
-    Leftof -> let c1 = fromMaybe (-1) $ clmn 0 i1 w
-                  c2 = fromMaybe (-1) $ clmn 0 i2 w
-              in (c1 /= (-1) && c2 /= (-1)) && c1 < c2
+    Ontop   -> isOver i1 i2 True w
+    Inside  -> isOver i1 i2 True w
+    Beside  -> isBeside i1 i2 w
+    Leftof  -> let c1 = fromMaybe (-1) $ clmn 0 i1 w
+                   c2 = fromMaybe (-1) $ clmn 0 i2 w
+               in (c1 /= (-1) && c2 /= (-1)) && c1 < c2
     Rightof -> let c1 = fromMaybe (-1) $ clmn 0 i1 w
                    c2 = fromMaybe (-1) $ clmn 0 i2 w
                in ((c1 /= (-1) && c2 /= (-1)) && c1 > c2)
-    Above -> isOver i1 i2 False w
-    Under -> isOver i2 i1 False w
-  MoveGoal {}      -> error "MoveGoal not fully implemented yet"
+    Above   -> isOver i1 i2 False w
+    Under   -> isOver i2 i1 False w
+  MoveGoal {} -> error "MoveGoal not fully implemented yet"
 
 -- | Checks if the second object is in the column strictly beside the first
 -- objects column.
 isBeside :: Id -> Id -> World -> Bool
-isBeside _ _ [] = False
-isBeside _ _ [_] = False
+isBeside _  _  []       = False
+isBeside _  _  [_]      = False
 isBeside i1 i2 (w:v:ws) = if isHere i1 i2 w
                             then isHere i1 i2 v
                             else isBeside i1 i2 (v:ws)
   where
-    isHere _ _ [] = False
+    isHere _ _ []     = False
     isHere i j (o:os) = (i == o || j == o) || isHere i j os
 
