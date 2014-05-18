bcca26535ac458239901ed76a493d835a4168ed5
Sebastian Lagermann <barafinkel@gmail.com>
2014-05-18 11:01:05 +0200

    Cleaned up the code a bit to make it more readable.

--- haskell/src/Shrdlite/AStar.hs
+++ haskell/src/Shrdlite/AStar.hs
@@ -29,10 +29,10 @@ data AStarState = AStarState { closed   :: !(S.Set WorldHolding)
 -- | Find the best path from the initial node to (one of) the goal state(s).
 -- Gives this in the form of a 'Plan'.
 aStar :: (WorldHolding -> S.Set WorldHolding) -- ^ Graph to search through
-        -> (WorldHolding -> Int)              -- ^ Heuristic distance to the goal
-        -> (WorldHolding -> Bool)             -- ^ Goal check function
-        -> WorldHolding                       -- ^ Starting point
-        -> Maybe Plan                         -- ^ Resulting path if it exists
+      -> (WorldHolding -> Int)                -- ^ Heuristic distance to the goal
+      -> (WorldHolding -> Bool)               -- ^ Goal check function
+      -> WorldHolding                         -- ^ Starting point
+      -> Maybe Plan                           -- ^ Resulting path if it exists
 aStar graph heur check start =
     aStar' initState graph heur check
   where initState = AStarState {closed = S.empty,
@@ -42,28 +42,28 @@ aStar graph heur check start =
 
 -- | Much like 'aStar', but also takes an initial state
 aStar' :: AStarState                            -- ^ Initial state
-        -> (WorldHolding -> S.Set WorldHolding) -- ^ Graph to search through
-        -> (WorldHolding -> Int)                -- ^ Heuristic distance to the goal
-        -> (WorldHolding -> Bool)               -- ^ Goal check function
-        -> Maybe Plan                           -- ^ Resulting path if it exists
+       -> (WorldHolding -> S.Set WorldHolding)  -- ^ Graph to search through
+       -> (WorldHolding -> Int)                 -- ^ Heuristic distance to the goal
+       -> (WorldHolding -> Bool)                -- ^ Goal check function
+       -> Maybe Plan                            -- ^ Resulting path if it exists
 aStar' s g h c = case Q.minView $ open s of -- take best open node
-    Just (node :-> _, open') -> -- the chosen node and updated open queue
-        if c node -- the goal is found
-        then Just $ resultList node $ parent state' -- find result here
-        else aStar' state' g h c -- run astar' with updated state
+    Just (node :-> _, open') ->       -- the chosen node and updated open queue
+        if c node                     -- the goal is found
+          then Just $ resultList node $ parent state' -- find result here
+          else aStar' state' g h c    -- run astar' with updated state
       where
         state' = S.fold (updateState h node) (s {closed=closed', open=open'}) newNodes
-        closed' = S.insert node $ closed s -- add node to the closed list
+        closed' = S.insert node $ closed s      -- add node to the closed list
         newNodes = S.difference (g node) closed' -- adjacent nodes not visited
-    Nothing -> Nothing -- open queue empty, no result found
+    Nothing -> Nothing                    -- open queue empty, no result found
 
 -- | Update the state with a new node, unless it is already in open with a
 -- cheaper cost
-updateState :: (WorldHolding -> Int)  -- ^ Heuristic function
-            -> WorldHolding           -- ^ Previous node
-            -> WorldHolding           -- ^ Current node
-            -> AStarState             -- ^ State to update
-            -> AStarState             -- ^ New state
+updateState :: (WorldHolding -> Int) -- ^ Heuristic function
+            -> WorldHolding          -- ^ Previous node
+            -> WorldHolding          -- ^ Current node
+            -> AStarState            -- ^ State to update
+            -> AStarState            -- ^ New state
 updateState h previous x state = if pCost > oldCost
     then state -- do nothing if the path to get to the state is longer
     else state {open=open', pathCost=pathCost', parent=parent'}
